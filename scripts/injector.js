1015775b7823d308bb51dda3a003b656f976752c
ddshiyu <jiandong@yzjtech.com>
2021-02-27 19:36:17 +0800

    爱心

--- scripts/injector.js
+++ scripts/injector.js
@@ -7,6 +7,12 @@ hexo.extend.injector.register(
   <script src="//cdn.jsdelivr.net/npm/minigrid@3.1.1/dist/minigrid.min.js"></script>
   <script src="https://cdn.staticfile.org/fancybox/3.5.7/jquery.fancybox.min.js"></script>
 	<script src="https://cdn.bootcdn.net/ajax/libs/lazyloadjs/3.2.2/lazyload.js"></script>
-    <script defer src="${siteRoot}js/photoWall.js"></script>`,
+  <script defer src="${siteRoot}js/photoWall.js"></script>`,
   "photo"
 );
+
+hexo.extend.injector.register(
+  "body_end",
+  `<script type="text/javascript" src="${siteRoot}js/love.js"></script>`,
+  "default"
+)
